ed091e445cecd21b056facb9cb09b92c9573da2d
HuyPV_99 <74908677+Phamvanhuy999@users.noreply.github.com>
2022-02-19 18:17:07 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import React, { useState } from 'react'
+import './App.css'
 function App() {
-  return (
-    <div className="App">
-      <h1>Todo List</h1>
+  const [newtask, setNewTask] = useState([])
+  const [tasks, setTasks] = useState([])
+  function addtask() {
+    setTasks([...tasks, newtask])
+    
+  }
+  function deletetask(index) {
+    var duplicatearray = [...tasks]
+    duplicatearray.splice(index, 1)
+    setTasks(duplicatearray)
+    
+  }
+  const taskslist = tasks.map((object, index) => {
+    return <div className='row justify-content-center'>
+      <h1 className='col-md-6 '>{index + 1}. {object}</h1>
+      <button onClick={() => { deletetask(index) }} className='btn btn-danger col-md-1 mb-2' >Delete</button>
     </div>
-  );
+  })
+  return (
+    <div className='App'>
+      <h1>Todo List</h1>
+      <div className='row justify-content-center'>
+        <input type='text'
+          placeholder='taskname'
+          className='form-control '
+          value={newtask}
+          onChange={(e) => { setNewTask(e.target.value) }}
+        />
+        <button onClick={addtask} className='btn btn-success col-md-1 m-1'>Add</button>
+
+      </div>
+      {taskslist}
+    </div>
+  )
 }
 
-export default App;
+export default App
